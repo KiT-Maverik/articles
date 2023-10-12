33b34523c48e2ec4345eaa86a5d9f870a9294ba0
kit <kit.mavareik@gmail.com>
2023-10-12 23:07:33 +0300

    ArticleCard style tweaks

--- src/components/article-card/article-card.style.ts
+++ src/components/article-card/article-card.style.ts
@@ -3,6 +3,8 @@ import {type SxProps} from "@mui/system";
 const container: SxProps = {
     minWidth: 180,
     maxWidth: 400,
+    display: "flex",
+    flexDirection: "column",
 } as const;
 
 const media: SxProps = {
@@ -10,7 +12,23 @@ const media: SxProps = {
     aspectRatio: 4 / 3,
 } as const;
 
+const contentContainer: SxProps = {
+    display: "flex",
+    flexDirection: 'column',
+    gap: 3,
+    flexGrow: 1,
+} as const;
+
+const description: SxProps = {
+    textAlign: "justify",
+    flexGrow: 1
+} as const;
+
 export const style = {
     container,
     media,
+    content: {
+        container: contentContainer,
+        description,
+    }
 }
--- src/components/article-card/article-card.tsx
+++ src/components/article-card/article-card.tsx
@@ -1,10 +1,11 @@
-import {Card, CardMedia, CardContent, CardActions} from "@mui/material";
+import {Card, CardMedia, CardContent} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 import {Chip} from "@mui/material";
 
 import {style} from "./article-card.style";
 import {I_Article} from "src/types/content.types";
+import {Stack} from "@mui/system";
 
 export const ArticleCard = ({id, title, description, cover, tags}: I_Article) => {
 
@@ -15,17 +16,17 @@ export const ArticleCard = ({id, title, description, cover, tags}: I_Article) =>
     return (
         <Card sx={style.container}>
             <CardMedia image={cover} sx={style.media}/>
-            <CardContent>
+            <CardContent sx={style.content.container}>
                 <Typography variant="h5">
                     {title}
                 </Typography>
-                <Typography variant="body2">
+                <Typography variant="body2" sx={style.content.description}>
                     {description}
                 </Typography>
+                <Stack direction="row" gap={2}>
+                    {renderTags()}
+                </Stack>
             </CardContent>
-            <CardActions>
-                {renderTags()}
-            </CardActions>
         </Card>
     );
 };
