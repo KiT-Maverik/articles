8980420bbbd0ec9fd2e7560afbf0c367c7b89371
kit <kit.mavareik@gmail.com>
2023-10-12 19:38:19 +0300

    Footer

--- src/components/layout/footer/footer.style.ts
+++ src/components/layout/footer/footer.style.ts
@@ -1,95 +1,26 @@
 import {blueGrey} from "@mui/material/colors";
-import {type SxProps, type Theme} from "@mui/system";
-
-import {theme} from "@styles/theme.styles";
-import {container as containerLayout} from "@styles/utility.styles";
-
-const area = {
-    about: 'about',
-    customerCare: 'customerCare',
-    logo: 'logo',
-    socials: 'socials',
-} as const;
+import {type SxProps} from "@mui/system";
 
 const background: SxProps = {
     backgroundColor: blueGrey[900],
 } as const;
 
-const container: SxProps<Theme> = {
-    ...containerLayout,
-
-    display: 'grid',
-    gap: 4,
-    my: 10,
-    gridTemplateColumns: '1fr 1fr',
-    gridTemplateAreas: `
-    '${area.about} ${area.customerCare}'
-    '${area.logo} ${area.logo}'
-    '${area.socials} ${area.socials}'`,
-
-    ul:  {
-        margin: 0,
-        padding: 0,
-    },
-
-    'ul li' : {
-        listStyle: 'none',        
-    },
-
-    a: {
-        textDecoration: 'none',
-        color: theme => theme.palette.common.white,        
-    },    
-
-    h5: {
-        color: theme => theme.palette.common.white,
-        textAlign: 'center',
-    },
-
-    button: {
-        color: theme => theme.palette.common.white,
-    },
-
-    [theme.breakpoints.up('desktop')]: {
-        gridTemplateColumns: '1fr 1fr 1fr 1fr',
-        gridTemplateAreas: `
-            '${area.logo} ${area.about} ${area.customerCare} ${area.socials}'`,
-    }
+const container: SxProps = {
+    py: 10,
 } as const;
 
-const about: SxProps = {
-    gridArea: area.about,
+const divider: SxProps = {
+    borderColor: 'white'
 } as const;
 
-const customerCare: SxProps = {
-    gridArea: area.customerCare,
-} as const;
-
-const logo: SxProps<Theme> = {
-    gridArea: area.logo,
-    display: "flex",
-    justifyContent: 'center',
-    maxWidth: '272px',
-
-    img: {
-        maxWidth: '100%',
-        height: 'auto',
-    }
-} as const;
-
-const socials: SxProps = {
-    gridArea: area.socials,
-    display: "flex",
-    justifyContent: 'center',
-    alignItems: "center",
-    gap: 2,
+const link: SxProps = {
+    textTransform: 'capitalize',
+    color: 'white',
 } as const;
 
 export const style = {
     background,
     container,
-    about,
-    customerCare,
-    logo,
-    socials,
+    divider,
+    link,
 }
--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,12 +1,25 @@
 import Box from "@mui/material/Box";
 
 import {style} from "./footer.style";
+import {Button, Divider} from "@mui/material";
+import {route} from "@constants/page-routes.constants";
+import {useRouter} from "next/router";
+import {Stack} from "@mui/system";
 
 export const Footer = () => {
+    const {push} = useRouter();
 
     return (
         <Box component='footer' sx={style.background}>
-
+            <Stack
+                direction='row'
+                justifyContent='center'
+                sx={style.container}
+                divider={<Divider orientation="vertical" flexItem sx={{borderColor: 'white'}}/>}
+            >
+                <Button onClick={() => push(route.placeholder)} sx={style.link}>Terms and conditions</Button>
+                <Button onClick={() => push(route.placeholder)} sx={style.link}>Privacy policy</Button>
+            </Stack>
         </Box>
     );
 }
