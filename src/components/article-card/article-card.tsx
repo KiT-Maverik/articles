df2ebc353f663e2b39b8abae12b4d2bf1d656b00
kit <kit.mavareik@gmail.com>
2023-10-13 03:27:23 +0300

    Introducing click handler into ArticleCard

--- src/components/article-card/article-card.tsx
+++ src/components/article-card/article-card.tsx
@@ -6,15 +6,18 @@ import {Chip} from "@mui/material";
 import {style} from "./article-card.style";
 import {I_Article} from "src/types/content.types";
 import {Stack} from "@mui/system";
+import {route} from "@constants/page-routes.constants";
+import {useRouter} from "next/router";
 
 export const ArticleCard = ({id, title, description, cover, tags}: I_Article) => {
+    const {push} = useRouter();
 
     const renderTags = () => {
         return tags.map(item => <Chip label={item} key={item}/>)
     }
 
     return (
-        <Card sx={style.container}>
+        <Card sx={style.container} onClick={() => push(route.article(id))}>
             <CardMedia image={cover} sx={style.media}/>
             <CardContent sx={style.content.container}>
                 <Typography variant="subtitle1" color="primary">
--- src/constants/page-routes.constants.ts
+++ src/constants/page-routes.constants.ts
@@ -1,4 +1,5 @@
 export const route = {
     home: '/',
     placeholder: '#',
+    article: (id: number) => `/article/${id}`,
 } as const;
